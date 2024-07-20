d01807f8a14a4a40662af79add943600d11925ac
Pastor Cortes Osorno <PCO07@hotmail.com>
2024-07-20 10:17:40 -0600

    Demo simple para diagnosticar los mensajes de error en metodos que imlementan TAP en un nivel mas bajo de ejecución. Logging apropiado y demostracion de manejo de error.

---
+++ FirstDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program 
+{
+    public static int x = 20;
+    public static int y = 0;
+
+    public static async Task Main(string[] args) 
+    {
+        try
+        {
+            var magicNumber = await GetMagicNumber();
+
+            await GetMagicMessage();
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        
+    }
+
+    private static Task<double> GetMagicNumber() 
+    {
+        try
+        {
+            var number = Convert.ToDouble(x / y);
+            Console.WriteLine("The magic number is: " + number);
+            return Task.FromResult(number);
+        }
+        catch (Exception ex) 
+        {
+            Console.Write(ex.ToString());
+            return null;
+        }
+
+    }
+
+    private static async Task GetMagicMessage()
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine("The second Task was executed");
+    }
+
+
+
+}
